ae9b58bf59def4d2b954588b3da1d8a1de38b607
Jenny Rotimi <jennyrotimi365@gmail.com>
2025-09-12 09:08:49 +0100

    connect page

--- connect.php
+++ connect.php
@@ -1,3 +1,14 @@
 <?php
 
+$hostname = "localhost";
+$username = "Jenny"; 
+$password = "2345";
+$database = "recipes_tb";
+
+// creating a connection to the db
+$connect = mysqli_connect ($hostname, $username, $password, $database);
+
+if($connect){
+  echo "success";
+};
 ?>
